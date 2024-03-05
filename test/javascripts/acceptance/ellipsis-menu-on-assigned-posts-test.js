8ce653d7da8fe252b6d242f1dc4076ba309d989c
Andrei Prigorshnev <a.prigorshnev@gmail.com>
2024-03-05 20:30:25 +0400

    WIP: tests

--- test/javascripts/acceptance/ellipsis-menu-on-assigned-posts-test.js
+++ test/javascripts/acceptance/ellipsis-menu-on-assigned-posts-test.js
@@ -6,38 +6,29 @@ import {
   query,
   updateCurrentUser,
 } from "discourse/tests/helpers/qunit-helpers";
-import selectKit from "discourse/tests/helpers/select-kit-helper";
 import { cloneJSON } from "discourse-common/lib/object";
-import I18n from "I18n";
-import NotificationFixture from "../fixtures/notifications-fixtures";
 
 function assignCurrentUserToTopic(needs) {
   needs.pretender((server, helper) => {
     server.get("/t/44.json", () => {
+      const username = "eviltrout";
+
       const topic = cloneJSON(topicFixtures["/t/28830/1.json"]);
-      console.log("topic", topic);
-      // topic["assigned_to_user"] = {
-      //   username: "eviltrout",
-      //   name: "Robin Ward",
-      //   avatar_template:
-      //     "/letter_avatar/eviltrout/{size}/3_f9720745f5ce6dfc2b5641fca999d934.png",
-      // };
-      // topic["assignment_note"] = "Shark Doododooo";
-      // topic["assignment_status"] = "New";
-      // topic["indirectly_assigned_to"] = {
-      //   2: {
-      //     assigned_to: {
-      //       name: "Developers",
-      //     },
-      //     post_number: 2,
-      //     assignment_note: '<script>alert("xss")</script>',
-      //   },
-      // };
-      topic.post_stream.posts[1]["assigned_to_user"] = {
-          username: "eviltrout",
-          name: "Robin Ward",
-          avatar_template:
-            "/letter_avatar/eviltrout/{size}/3_f9720745f5ce6dfc2b5641fca999d934.png",
+      const secondPost = topic.post_stream.posts[1];
+      topic["indirectly_assigned_to"] = {
+        118597: {
+          assigned_to: {
+            username,
+          },
+          post_number: 1,
+        },
+      };
+
+      secondPost["assigned_to_user"] = {
+        username,
+        name: "Robin Ward",
+        avatar_template:
+          "/letter_avatar/eviltrout/{size}/3_f9720745f5ce6dfc2b5641fca999d934.png",
       };
 
       return helper.response(topic);
@@ -45,43 +36,45 @@ function assignCurrentUserToTopic(needs) {
   });
 }
 
-acceptance("Discourse Assign | Ellipsis menu on assigned posts", function (needs) {
-  needs.user();
-  needs.settings({
-    assign_enabled: true,
-    tagging_enabled: true,
-    assigns_user_url_path: "/",
-    assigns_public: true,
-    enable_assign_status: true,
-  });
+acceptance(
+  "Discourse Assign | Ellipsis menu on assigned posts",
+  function (needs) {
+    needs.user();
+    needs.settings({
+      assign_enabled: true,
+      tagging_enabled: true,
+      assigns_user_url_path: "/",
+      assigns_public: true,
+      enable_assign_status: true,
+    });
 
-  assignCurrentUserToTopic(needs);
+    assignCurrentUserToTopic(needs);
 
-  test("Unassigns the post", async function (assert) {
-    updateCurrentUser({ can_assign: true });
-    await visit("/t/assignment-topic/44");
-    console.log("breakpoint");
-    await click(".more-button");
-    // await click(".post-stream .topic-post .more-button");
+    test("Unassigns the post", async function (assert) {
+      updateCurrentUser({ can_assign: true });
+      await visit("/t/assignment-topic/44");
+      console.log("breakpoint");
+      await click(".more-button");
+      // await click(".post-stream .topic-post .more-button");
 
+      assert
+        .dom("#topic-title .assigned-to")
+        .hasText("eviltrout", "shows assignment in the header");
+      assert
+        .dom("#topic-footer-dropdown-reassign")
+        .exists("shows reassign dropdown at the bottom of the topic");
+    });
 
-    assert
-      .dom("#topic-title .assigned-to")
-      .hasText("eviltrout", "shows assignment in the header");
-    assert
-      .dom("#topic-footer-dropdown-reassign")
-      .exists("shows reassign dropdown at the bottom of the topic");
-  });
+    test("Reassigns the post", async function (assert) {
+      updateCurrentUser({ can_assign: true });
+      await visit("/t/assignment-topic/44");
 
-  test("Reassigns the post", async function (assert) {
-    updateCurrentUser({ can_assign: true });
-    await visit("/t/assignment-topic/44");
-
-    assert
-      .dom("#topic-title .assigned-to")
-      .hasText("eviltrout", "shows assignment in the header");
-    assert
-      .dom("#topic-footer-dropdown-reassign")
-      .exists("shows reassign dropdown at the bottom of the topic");
-  });
-});
+      assert
+        .dom("#topic-title .assigned-to")
+        .hasText("eviltrout", "shows assignment in the header");
+      assert
+        .dom("#topic-footer-dropdown-reassign")
+        .exists("shows reassign dropdown at the bottom of the topic");
+    });
+  }
+);
