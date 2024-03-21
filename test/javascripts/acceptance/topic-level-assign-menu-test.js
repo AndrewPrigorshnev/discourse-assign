72f9871001c16bc1b7bddf75b791318611227933
Andrei Prigorshnev <a.prigorshnev@gmail.com>
2024-03-21 22:47:45 +0400

    Test

--- test/javascripts/acceptance/topic-level-assign-menu-test.js
+++ test/javascripts/acceptance/topic-level-assign-menu-test.js
@@ -1,48 +1,13 @@
-import { click, fillIn, pauseTest, visit } from "@ember/test-helpers";
+import { click, visit } from "@ember/test-helpers";
 import { test } from "qunit";
-import topicFixtures from "discourse/tests/fixtures/topic";
 import {
   acceptance,
   publishToMessageBus,
   updateCurrentUser,
 } from "discourse/tests/helpers/qunit-helpers";
-import { cloneJSON } from "discourse-common/lib/object";
+import topicWithAssignedPost from "../fixtures/topic-with-assigned-post";
 
-const username = "eviltrout";
-const new_assignee_username = "new_assignee";
-
-function topicWithAssignedPostResponse() {
-  const topic = cloneJSON(topicFixtures["/t/28830/1.json"]);
-  const secondPost = topic.post_stream.posts[1];
-
-  topic["indirectly_assigned_to"] = {
-    [secondPost.id]: {
-      assigned_to: {
-        username,
-      },
-      post_number: 1,
-    },
-  };
-  secondPost["assigned_to_user"] = { username };
-
-  return topic;
-}
-
-const selectors = {
-  assignedTo: ".post-stream article#post_2 .assigned-to",
-  moreButton: ".post-stream .topic-post .more-button",
-  popupMenu: {
-    unassign: ".popup-menu .popup-menu-btn svg.d-icon-user-plus",
-    editAssignment: ".popup-menu .popup-menu-btn svg.d-icon-group-plus",
-  },
-  modal: {
-    assignee: ".modal-container .select-kit-header-wrapper",
-    assigneeInput: ".modal-container .filter-input",
-    assignButton: ".d-modal__footer .btn-primary",
-  },
-};
-
-const topic = topicWithAssignedPostResponse();
+const topic = topicWithAssignedPost();
 const post = topic.post_stream.posts[1];
 
 acceptance("Discourse Assign | Topic level assign menu", function (needs) {
@@ -53,40 +18,20 @@ acceptance("Discourse Assign | Topic level assign menu", function (needs) {
 
   needs.pretender((server, helper) => {
     server.get("/t/44.json", () => helper.response(topic));
-
-    server.put("/assign/assign", () => {
-      return helper.response({ success: true });
-    });
-
     server.put("/assign/unassign", () => {
       return helper.response({ success: true });
     });
-
-    server.get("/assign/suggestions", () =>
-      helper.response({
-        assign_allowed_for_groups: [],
-        suggestions: [{ username: new_assignee_username }],
-      })
-    );
-
-    server.get("/u/search/users", () =>
-      helper.response({ users: [{ username: new_assignee_username }] })
-    );
   });
 
   needs.hooks.beforeEach(() => {
     updateCurrentUser({ can_assign: true });
   });
 
-  test("Unassigns the post", async function (assert) {
+  test("Unassign button unassigns the post", async function (assert) {
     await visit("/t/assignment-topic/44");
-    await pauseTest();
 
-    await click("#topic-footer-dropdown-reassign");
-
-    debugger;
-
-    await click(selectors.popupMenu.unassign);
+    await click("#topic-footer-dropdown-reassign .btn");
+    await click(`li[data-value='unassign-from-post-${post.id}']`);
     await publishToMessageBus("/staff/topic-assignment", {
       type: "unassigned",
       topic_id: topic.id,
@@ -94,7 +39,11 @@ acceptance("Discourse Assign | Topic level assign menu", function (needs) {
       assigned_type: "User",
     });
 
-    assert.dom(".popup-menu").doesNotExist("The popup menu is closed");
-    assert.dom(selectors.assignedTo).doesNotExist("The post is unassigned");
+    assert
+      .dom("#topic-footer-dropdown-reassign-body ul[role='menu']")
+      .doesNotExist("The menu is closed");
+    assert
+      .dom(".post-stream article#post_2 .assigned-to")
+      .doesNotExist("The post is unassigned");
   });
 });
