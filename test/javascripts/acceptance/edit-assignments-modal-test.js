ea8e4e19801ae1898600bc0b7be2c905d1013585
Andrei Prigorshnev <a.prigorshnev@gmail.com>
2024-04-12 00:19:00 +0400

    Assign two posts in the test case

--- test/javascripts/acceptance/edit-assignments-modal-test.js
+++ test/javascripts/acceptance/edit-assignments-modal-test.js
@@ -11,6 +11,7 @@ import topicWithAssignedPosts from "../fixtures/topic-with-assigned-posts";
 
 const topic = topicWithAssignedPosts();
 const firstReply = topic.post_stream.posts[1];
+const secondReply = topic.post_stream.posts[2];
 const new_assignee_1 = "user_1";
 const new_assignee_2 = "user_2";
 
@@ -54,9 +55,14 @@ acceptance("Discourse Assign | Edit assignments modal", function (needs) {
     await expandAssigneeChooser();
     await selectAssignee(new_assignee_2);
 
+    await selectPost(2);
+    await expandAssigneeChooser();
+    await selectAssignee(new_assignee_2);
+
     await submitModal();
     await receiveTopicAssignedMessage(new_assignee_1, appEvents);
     await receivePostAssignedMessage(firstReply, new_assignee_2, appEvents);
+    await receivePostAssignedMessage(secondReply, new_assignee_2, appEvents);
 
     assert
       .dom(".post-stream article#post_1 .assigned-to .assigned-to--user a")
@@ -65,6 +71,10 @@ acceptance("Discourse Assign | Edit assignments modal", function (needs) {
     assert
       .dom(".post-stream article#post_2 .assigned-to .assigned-to-username")
       .hasText(new_assignee_2, "The first reply is assigned to a new assignee");
+
+    assert
+      .dom(".post-stream article#post_3 .assigned-to .assigned-to-username")
+      .hasText(new_assignee_2, "The second reply is assigned to a new assignee");
   });
 
   async function expandAssigneeChooser() {
