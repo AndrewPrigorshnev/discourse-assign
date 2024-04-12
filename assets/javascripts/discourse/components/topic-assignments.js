ee4e489230129aecfc4cc00bfa3e3505ab4feabb
Andrei Prigorshnev <a.prigorshnev@gmail.com>
2024-04-12 15:30:39 +0400

    Refactor

--- assets/javascripts/discourse/components/topic-assignments.js
+++ assets/javascripts/discourse/components/topic-assignments.js
@@ -16,27 +16,27 @@ export default class TopicAssignments extends Component {
   }
 
   @action
-  selectAssignment(selectedAssignmentId) {
-    this.selectedAssignmentId = selectedAssignmentId;
-    // fixme andrei:
-    if (selectedAssignmentId === 0) {
+  selectAssignment(id) {
+    this.selectedAssignmentId = id;
+    if (id === 0) {
       this.selectedAssignment = this.args.assignments.find(
         (a) => a.targetType === "Topic"
       );
     } else {
       this.selectedAssignment = this.args.assignments.find(
-        (a) => a.postNumber === selectedAssignmentId
+        (a) => a.postNumber === id
       );
     }
   }
 
   #toComboBoxOption(assignment) {
+    // fixme andrei strings
     if (assignment.targetType === "Topic") {
       return { id: 0, name: "Topic" };
     } else {
       return {
         id: assignment.postNumber,
-        name: `Post #${assignment.postNumber}`, // fixme andrei strings
+        name: `Post #${assignment.postNumber}`,
       };
     }
   }
